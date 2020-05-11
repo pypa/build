2bb42d3b516a4f8c28a8c466f17fb8f432a4ba8e
Filipe Laíns <lains@archlinux.org>
2020-05-11 02:43:27 +0100

    build: refactor version identification
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- build/__init__.py
+++ build/__init__.py
@@ -8,10 +8,10 @@ __version__ = '0.0.1'
 import importlib
 import importlib_metadata
 import os
+import platform
 import re
-import sys
 
-from typing import List
+from typing import Callable, List
 
 import pep517.wrappers
 import toml
@@ -27,6 +27,81 @@ class BuildBackendException(Exception):
     '''
     Exception raised when the backend fails
     '''
+
+
+class VersionChecker(object):
+    @classmethod
+    def _compare_version_strings(cls, val1, val2, operation):  # type: (str, str, str) -> bool
+        ops = {
+            '<=': 'le',
+            '>=': 'ge',
+            '<': 'lt',
+            '>': 'gt',
+            '==': 'eq',
+            '!=': 'ne',
+        }
+
+        # TODO: validate
+        v1 = val1.split('.')
+        v2 = val2.split('.')
+
+        length = len(val1) if len(val1) < len(val2) else len(val2)
+
+        v1 = v1[:length]
+        v2 = v2[:length]
+
+        try:
+            op = getattr(v1, '__{}__'.format(ops[operation]))  # type: Callable[[List[str]], bool]
+            return op(v2)
+        except (AttributeError, KeyError, TypeError):
+            raise BuildException("Operation '{}' (from '{} {} {}') not supported".format(operation, val1, operation, val2))
+
+    @classmethod
+    def check_version(cls, requirement_string, env=False):  # type: (str, bool) -> bool  # noqa: 901
+        ops = {
+            '<=': 'le',
+            '>=': 'ge',
+            '<': 'lt',
+            '>': 'gt',
+            '==': 'eq',
+            '!=': 'ne',
+        }
+
+        reqs = requirement_string.split(';')
+
+        if len(reqs) > 1:
+            for req in reqs[:0:-1]:  # loop over conditions in reverse
+                if not cls.check_version(req, env=True):
+                    return True
+
+        explode_req = re.compile('({})'.format('|'.join(ops.keys())))
+        fields = explode_req.split(re.sub('[ \'"]', '', reqs[0].strip()))  # ['something, '>=', '0.3']
+
+        if not fields or len(fields) % 2 == 0:  # ['something', '>=']
+            raise BuildException('Invalid dependency string: {}'.format(requirement_string))
+
+        name = fields[0]
+
+        if env:
+            if name == 'python_version':
+                version = '.'.join(platform.python_version_tuple()[:2])
+            else:
+                raise BuildException('Invalid environment marker: {}'.format(name))
+        else:
+            try:
+                version = importlib_metadata.version(name).split('.')
+            except importlib_metadata.PackageNotFoundError:
+                return False
+
+        if len(fields) == 0:
+            return True
+
+        i = 1
+        while i < len(fields):
+            cls._compare_version_strings(version, fields[i + 1], fields[i])
+            i += 2
+
+        return True
 
 
 class ProjectBuilder(object):
@@ -55,57 +130,6 @@ class ProjectBuilder(object):
         self.hook = pep517.wrappers.Pep517HookCaller(self.srcdir, self._backend,
                                                      backend_path=self._build_system.get('backend-path'))
 
-    @classmethod
-    def check_version(cls, requirement_string, env=False):  # type: (str, bool) -> bool  # noqa: 901
-        ops = {
-            '<=': 'le',
-            '>=': 'ge',
-            '<': 'lt',
-            '>': 'gt',
-            '==': 'eq',
-            '!=': 'ne',
-        }
-
-        reqs = requirement_string.split(';')
-
-        if len(reqs) > 1:
-            for req in reqs[:0:-1]:  # loop over conditions in reverse
-                if not cls.check_version(req, env=True):
-                    return True
-
-        explode_req = re.compile('({})'.format('|'.join(ops.keys())))
-        fields = explode_req.split(re.sub('[ \'"]', '', reqs[0].strip()))  # ['something, '>=', '0.3']
-
-        if not fields or len(fields) % 2 == 0:  # ['something', '>=']
-            raise BuildException('Invalid dependency string: {}'.format(requirement_string))
-
-        if env:
-            if fields[0] == 'python':
-                version = [str(v) for v in sys.version_info[:3]]
-        else:
-            try:
-                version = importlib_metadata.version(fields[0]).split('.')
-            except importlib_metadata.PackageNotFoundError:
-                return False
-
-        if len(fields) == 0:
-            return True
-
-        i = 1
-        while i < len(fields):
-            target = fields[i + 1].split('.')
-            lenght = len(version) if len(version) < len(target) else len(target)
-            local_version = version[:lenght]
-            try:
-                op = getattr(local_version, '__{}__'.format(ops[fields[i]]))
-                if not op(target[:lenght]):
-                    return False
-            except (AttributeError, KeyError, TypeError):
-                raise BuildException("Operation '{}' (from '{}') not supported".format(fields[i], requirement_string))
-            i += 2
-
-        return True
-
     def check_depencencies(self, distribution):  # type: (str) -> List[str]
         '''
         Returns a set of the missing dependencies
@@ -123,7 +147,7 @@ class ProjectBuilder(object):
 
         missing = []
         for dep in dependencies:
-            if not self.check_version(dep):
+            if not VersionChecker.check_version(dep):
                 missing.append(dep)
 
         return missing
