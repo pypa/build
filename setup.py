7738d41f0959c89c2724ab8ce0259df265dd04d1
Filipe Laíns <lains@archlinux.org>
2020-05-17 17:42:03 +0100

    add setup.py
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='build',
+    version='0.0.1',
+    project_urls={'homepage': 'https://github.com/FFY00/python-build'},
+    author='Filipe Laíns',
+    author_email='lains@archlinux.org',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ],
+    packages=['build'],
+    install_requires=[
+        'toml'
+        'pep517',
+        'packaging',
+        'importlib-metadata; python_version < "3.8"',
+    ],
+)
