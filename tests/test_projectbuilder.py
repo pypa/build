e52cb8a074db947b2cd9fc49266977774714f09c
Filipe Laíns <lains@archlinux.org>
2020-06-09 02:36:25 +0100

    tests: add tests for ProjectBuilder init
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- tests/test_projectbuilder.py
+++ tests/test_projectbuilder.py
@@ -2,8 +2,10 @@
 
 from __future__ import unicode_literals
 
+import importlib
 import sys
 
+import pep517.wrappers
 import pytest
 
 import build
@@ -17,11 +19,47 @@ else:  # pragma: no cover
     import importlib_metadata
     email_message_from_string = importlib_metadata._compat.email_message_from_string
 
+if sys.version_info >= (3,):  # pragma: no cover
+    build_open_owener = 'builtins'
+else:  # pragma: no cover
+    build_open_owener = 'build'
+    FileNotFoundError = IOError
+    PermissionError = OSError
+
 
 DUMMY_METADATA = '''
 Version: 1.0.0
 Provides-Extra: some_extra
 '''.strip()
+
+
+DUMMY_PYPROJECT = '''
+[build-system]
+build-backend = 'flit_core.buildapi'
+'''.strip()
+
+
+DUMMY_PYPROJECT_BAD = '''
+[build-system]
+build-backend = 'missing_backend'
+'''.strip()
+
+
+class MockDistribution(importlib_metadata.Distribution):
+    def read_text(self, filename):
+        return DUMMY_METADATA
+
+    def locate_file(self, path):  # pragma: no cover
+        return ''
+
+    @classmethod
+    def from_name(cls, name):
+        if name == 'something':
+            return cls()
+        raise importlib_metadata.PackageNotFoundError
+
+
+importlib_metadata.Distribution = MockDistribution
 
 
 @pytest.mark.parametrize(
@@ -41,19 +79,30 @@ Provides-Extra: some_extra
     ]
 )
 def test_check_version(requirement_string, extra, expected):
-    class MockDistribution(importlib_metadata.Distribution):
-        def read_text(self, filename):
-            return DUMMY_METADATA
-
-        def locate_file(self, path):  # pragma: no cover
-            return ''
-
-        @classmethod
-        def from_name(cls, name):
-            if name == 'something':
-                return cls()
-            raise importlib_metadata.PackageNotFoundError
-
-    importlib_metadata.Distribution = MockDistribution
-
     assert build.check_version(requirement_string) == expected
+
+
+def test_init(mocker):
+    open_mock = mocker.mock_open(read_data=DUMMY_PYPROJECT)
+    modules = {
+        'flit_core.buildapi': None,
+        'setuptools.build_meta:__legacy__': None,
+    }
+    mocker.patch('importlib.import_module', modules.get)
+    mocker.patch('{}.open'.format(build_open_owener), open_mock)
+    mocker.patch('pep517.wrappers.Pep517HookCaller')
+
+    # data = ''
+    build.ProjectBuilder('.')
+    pep517.wrappers.Pep517HookCaller.assert_called_with('.', 'flit_core.buildapi', backend_path=None)
+    pep517.wrappers.Pep517HookCaller.reset_mock()
+
+    # FileNotFoundError
+    open_mock.side_effect = FileNotFoundError
+    build.ProjectBuilder('.')
+    pep517.wrappers.Pep517HookCaller.assert_called_with('.', 'setuptools.build_meta:__legacy__', backend_path=None)
+
+    # PermissionError
+    open_mock.side_effect = PermissionError
+    with pytest.raises(build.BuildException):
+        build.ProjectBuilder('.')
