c02d54ecabc150e909bb28a1212b9c038a2428b9
Filipe Laíns <lains@archlinux.org>
2020-06-30 20:39:26 +0100

    tests: add test packages
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

---
+++ tests/packages/legacy/legacy/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: MIT
+
+'''
+legacy - Example legacy package
+'''
+__version__ = '1.0.0'
---
+++ tests/packages/test-flit/test_flit/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: MIT
+
+'''
+test_flit - Example flit package
+'''
+__version__ = '1.0.0'
