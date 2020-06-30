0839f3387561074ff7a131f8a8aaedd6d5424ad7
Filipe Laíns <lains@archlinux.org>
2020-06-30 21:36:53 +0100

    tests: use test packages instead of mocks
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,42 +1,50 @@
 # SPDX-License-Identifier: MIT
 
+import os
+import os.path
+import stat
+
 import pytest
 
 
-DUMMY_PYPROJECT = '''
-[build-system]
-requires = ['flit_core']
-build-backend = 'flit_core.buildapi'
-'''.strip()
-
-
-DUMMY_PYPROJECT_NO_BACKEND = '''
-[build-system]
-requires = []
-'''.strip()
-
-
-DUMMY_PYPROJECT_NO_REQUIRES = '''
-[build-system]
-build-backend = 'something'
-'''.strip()
+@pytest.fixture
+def packages_path():
+    return os.path.realpath(os.path.join(__file__, '..', 'packages'))
 
 
 @pytest.fixture
-def empty_file_mock(mocker):
-    return mocker.mock_open(read_data='')
+def legacy_path(packages_path):
+    return os.path.join(packages_path, 'legacy')
 
 
 @pytest.fixture
-def pyproject_mock(mocker):
-    return mocker.mock_open(read_data=DUMMY_PYPROJECT)
+def test_flit_path(packages_path):
+    return os.path.join(packages_path, 'test-flit')
 
 
 @pytest.fixture
-def pyproject_no_backend_mock(mocker):
-    return mocker.mock_open(read_data=DUMMY_PYPROJECT_NO_BACKEND)
+def test_bad_syntax_path(packages_path):
+    return os.path.join(packages_path, 'test-bad-syntax')
 
 
 @pytest.fixture
-def pyproject_no_requires_mock(mocker):
-    return mocker.mock_open(read_data=DUMMY_PYPROJECT_NO_REQUIRES)
+def test_no_backend_path(packages_path):
+    return os.path.join(packages_path, 'test-no-backend')
+
+
+@pytest.fixture
+def test_no_requires_path(packages_path):
+    return os.path.join(packages_path, 'test-no-requires')
+
+
+@pytest.fixture
+def test_no_permission(packages_path):
+    path = os.path.join(packages_path, 'test-no-permission')
+    file = os.path.join(path, 'pyproject.toml')
+    orig_stat = os.stat(file).st_mode
+
+    os.chmod(file, ~stat.S_IRWXU)
+
+    yield os.path.join(packages_path, 'test-no-permission')
+
+    os.chmod(file, orig_stat)
