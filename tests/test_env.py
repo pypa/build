177ce3ad6776d521967f1b6df61fca4cbc3841cc
Filipe Laíns <lains@archlinux.org>
2020-08-11 23:09:49 +0100

    tests: test the environment directories populated by IsolatedEnvironment
    This tests if the required environment directories are provided by
    IsolatedEnvironment.
    
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- tests/test_env.py
+++ tests/test_env.py
@@ -8,16 +8,43 @@ import sysconfig
 import build.env
 
 
-def test_isolated_environment(mocker):
+def test_isolated_environment_setup(mocker):
     old_path = os.environ['PATH']
     with build.env.IsolatedEnvironment() as env:
         if os.name != 'nt':
             assert os.environ['PATH'] == os.pathsep.join([os.path.join(env._path, 'bin'), old_path])
         assert os.environ['PYTHONHOME'] == env._path
 
+        python_path = os.environ['PYTHONPATH'].split(os.pathsep)
         for path in ('purelib', 'platlib'):
-            assert sysconfig.get_path(path) not in os.environ['PYTHONPATH'].split(os.pathsep)
+            assert sysconfig.get_path(path) not in python_path
+            assert sysconfig.get_path(
+                path,
+                vars={
+                    'base': env._path,
+                    'platbase': env._path,
+                }
+            ) in python_path
 
+        copy_path = (
+            sysconfig.get_path('include'),
+            sysconfig.get_path('platinclude'),
+            sysconfig.get_config_var('srcdir'),
+        )
+
+        prefix = sysconfig.get_config_var('prefix')
+        assert prefix is not None
+
+        for path in copy_path:
+            assert path is not None
+            if path.startswith(prefix):
+                relative_path = path[len(prefix + os.pathsep):]
+                path = os.path.join(env._path, relative_path)
+            assert os.path.exists(path)
+
+
+def test_isolated_environment_install(mocker):
+    with build.env.IsolatedEnvironment() as env:
         mocker.patch('subprocess.check_call')
 
         env.install([])
