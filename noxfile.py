ff990a8e67caae9e05ad78103770f777414f6aec
Filipe Laíns <lains@archlinux.org>
2020-09-07 21:25:27 +0100

    nox: test invoking/installing via PYTHONPATH, sdist and wheel
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,6 @@
 import os
+import os.path
+import tempfile
 
 import nox
 
@@ -15,14 +17,56 @@ def mypy(session):
     session.run('mypy', '--py2', 'src/build')
 
 
-@nox.session(python=['2.7', '3.5', '3.6', '3.7', '3.8', 'pypy2', 'pypy3'])
-def test(session):
+def run_tests(session, env=None):
     htmlcov_output = os.path.join(session.virtualenv.location, 'htmlcov')
     xmlcov_output = os.path.join(session.virtualenv.location, f'coverage-{session.python}.xml')
-
-    session.install('.[test]')
 
     session.run('pytest', '--cov', '--cov-config', 'setup.cfg',
                 f'--cov-report=html:{htmlcov_output}',
                 f'--cov-report=xml:{xmlcov_output}',
-                'tests/', *session.posargs)
+                'tests/', *session.posargs, env=env)
+
+
+@nox.session(python=['2.7', '3.5', '3.6', '3.7', '3.8', 'pypy2', 'pypy3'])
+def test(session):
+    session.install('.[test]')
+    run_tests(session)
+
+
+@nox.session(python=['2.7', '3.5', '3.6', '3.7', '3.8', 'pypy2', 'pypy3'])
+def test_pythonpath(session):
+    session.install('-r', 'requirements-dev.txt')
+    run_tests(session, env={'PYTHONPATH': 'src'})
+
+
+@nox.session(python=['2.7', '3.5', '3.6', '3.7', '3.8', 'pypy2', 'pypy3'])
+def test_wheel(session):
+    session.install('-r', 'requirements-dev.txt')
+
+    with tempfile.TemporaryDirectory(prefix='python-build-wheel-') as dest:
+        session.run(
+            'python', '-m', 'build', '--wheel', '--no-isolation', '--outdir', dest,
+            env={'PYTHONPATH': 'src'}
+        )
+        for target in os.listdir(dest):
+            if target.endswith('.whl'):
+                session.install(os.path.join(dest, target))
+                break
+
+    run_tests(session)
+
+
+@nox.session(python=['2.7', '3.5', '3.6', '3.7', '3.8', 'pypy2', 'pypy3'])
+def test_sdist(session):
+    session.install('-r', 'requirements-dev.txt')
+
+    with tempfile.TemporaryDirectory(prefix='python-build-wheel-') as dest:
+        session.run(
+            'python', '-m', 'build', '--sdist', '--no-isolation', '--outdir', dest,
+            env={'PYTHONPATH': 'src'}
+        )
+        for target in os.listdir(dest):
+            if target.endswith('.tar.gz'):
+                session.install(os.path.join(dest, target))
+
+    run_tests(session)
