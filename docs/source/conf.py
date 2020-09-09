d3b0f3d270a565bfdc8662f40ec4a96d69a7755f
Julian Berman <Julian@GrayVines.com>
2020-09-09 10:38:37 -0400

    doc: Set default_role, so now :option: can go away.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,6 +52,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+default_role = 'any'
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
