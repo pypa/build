de84dbc36fd2479c90978d9b52699e7ba9547ace
Bernát Gábor <bgabor8@bloomberg.net>
2020-10-12 18:36:32 +0100

    Improve integration tests
    - Make it parallel safe
    - Only download as much as needed instead of all
    - Move acquisition of the pkgs to where the test is
    - Allow running them without the --run-integration flag if only pytest
    target
    - Wrap URL into Request to make it work behind proxy firewall too
    (SSL_CERT_FILE set)
    - Building via pyproject-build is validated via existance of the console
    entrypoint rather than the existance of the PYTHONPATH variable (more
    accurate, and IDE friendly)
    - Ignore pip upgrade message for python 2
    - Use sys.executable -m build instead of python -m build to make it
    Windows friendly
    - Run integration tests after unit tests
    
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,24 +7,9 @@ import shutil
 import stat
 import subprocess
 import sys
-import tarfile
 import tempfile
 
-import filelock
 import pytest
-
-if sys.version_info[0] == 2:
-    from urllib2 import urlopen
-else:
-    from urllib.request import urlopen
-
-
-INTEGRATION_SOURCES = {
-    'dateutil': ('dateutil/dateutil', '2.8.1'),
-    'pip': ('pypa/pip', '20.2.1'),
-    'Solaar': ('pwr-Solaar/Solaar', '1.0.3'),
-    'flit': ('takluyver/flit', '2.3.0'),
-}
 
 
 def _setup():
@@ -65,8 +50,10 @@ def pytest_collection_modifyitems(config, items):
     if config.getoption('--run-integration') and config.getoption('--only-integration'):  # pragma: no cover
         raise pytest.UsageError("--run-integration and --only-integration can't be used together, choose one")
 
+    if len(items) == 1:  # do not require flags if called directly
+        return
     for item in items:
-        is_integration_file = os.path.basename(item.location[0]) == 'test_integration.py'
+        is_integration_file = is_integration(item)
         if PYPY3_WIN_VENV_BAD and item.get_closest_marker('isolated'):
             if not (is_integration_file and item.originalname == 'test_build') or (
                 hasattr(item, 'callspec') and '--no-isolation' not in item.callspec.params.get('args', [])
@@ -77,63 +64,17 @@ def pytest_collection_modifyitems(config, items):
                 item.add_marker(skip_int)
         elif config.getoption('--only-integration'):  # pragma: no cover
             item.add_marker(skip_other)
+    # run integration tests after unit tests
+    items.sort(key=lambda i: 1 if is_integration(i) else 0)
+
+
+def is_integration(item):
+    return os.path.basename(item.location[0]) == 'test_integration.py'
 
 
 @pytest.fixture
 def packages_path():
     return os.path.realpath(os.path.join(__file__, '..', 'packages'))
-
-
-@pytest.fixture(scope='session')
-def integration_path():
-    src_dir = os.path.realpath('.integration-sources')
-    dest = tempfile.mkdtemp(prefix='python-build-integration-')
-
-    if not os.path.exists(src_dir):
-        os.makedirs(src_dir)
-
-    # for python-build we use our own source directly
-    self_source = os.path.abspath(os.path.join(__file__, '..', '..'))
-    self_dest = os.path.join(dest, 'python-build')
-    if not os.path.exists(self_dest):
-        from build.env import fs_supports_symlink
-
-        if fs_supports_symlink():
-            os.symlink(self_source, self_dest)
-        else:  # pragma: no cover
-            os.makedirs(self_dest)
-            for target in ('pyproject.toml', 'setup.cfg', 'LICENSE', 'src'):
-                target_source = os.path.join(self_source, target)
-                target_dest = os.path.join(self_dest, target)
-                if os.path.isfile(target_source):
-                    shutil.copyfile(target_source, target_dest)
-                else:
-                    shutil.copytree(target_source, target_dest)
-
-    for target, (repo, version) in INTEGRATION_SOURCES.items():
-        with filelock.FileLock(os.path.join(src_dir, '{}.lock'.format(target))):
-            target_dest = os.path.join(dest, target)
-            if os.path.exists(target_dest):  # pragma: no cover
-                continue
-
-            tarball = os.path.join(src_dir, '{}.tar.gz'.format(target))
-            data = urlopen('https://github.com/{}/archive/{}.tar.gz'.format(repo, version)).read()
-            with open(tarball, 'wb') as f:
-                f.write(data)
-
-            tarfile.open(tarball, 'r:gz').extractall(dest)
-            shutil.move(os.path.join(dest, '{}-{}'.format(repo.split('/')[1], version)), target_dest)
-
-    yield dest
-
-    try:
-        os.unlink(self_dest)  # some implementations try to recursively remove the files inside the symlink
-        shutil.rmtree(dest)
-    except WindowsError:  # pragma: no cover
-        """
-        For some reason in some cases we don't have permission to remove
-        symlinks on windows, even though we created them?
-        """
 
 
 @pytest.fixture
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,55 +3,118 @@
 import os
 import os.path
 import re
+import shutil
 import subprocess
+import sys
+import tarfile
 
+if sys.version_info[0] == 3:
+    from urllib.request import Request, urlopen
+else:
+    from urllib2 import urlopen, Request
+
+import filelock
 import pytest
 
 import build.__main__
 
+INTEGRATION_SOURCES = {
+    'dateutil': ('dateutil/dateutil', '2.8.1'),
+    'pip': ('pypa/pip', '20.2.1'),
+    'Solaar': ('pwr-Solaar/Solaar', '1.0.3'),
+    'flit': ('takluyver/flit', '2.3.0'),
+}
+
 _SDIST = re.compile('.*.tar.gz')
 _WHEEL = re.compile('.*.whl')
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def get_project(name, tmp_path):
+    dest = tmp_path / name
+    if name == 'build':
+        # our own project is available in-source, just ignore development files
+
+        def _ignore_folder(_, filenames):
+            return [n for n in filenames if n in exclude or n.endswith('_cache') or n.endswith('.egg-info')]
+
+        exclude = '.tox', 'dist', 'build', '.git', '__pycache__'
+        shutil.copytree(ROOT, str(dest), ignore=_ignore_folder)
+        return dest
+
+    # for other projects download from github and cache it
+    tar_store = os.path.join(ROOT, '.integration-sources')
+    try:
+        os.makedirs(tar_store)
+    except OSError:  # python 2 has no exist_ok, and checking with exists is not parallel safe
+        pass  # just ignore, if the creation failed we will have another failure soon that will notify the user
+
+    github_org_repo, version = INTEGRATION_SOURCES[name]
+    tar_filename = '{}-{}.tar.gz'.format(name, version)
+    tarball = os.path.join(tar_store, tar_filename)
+    with filelock.FileLock(os.path.join(tar_store, '{}.lock'.format(tar_filename))):
+        if not os.path.exists(tarball):
+            url = 'https://github.com/{}/archive/{}.tar.gz'.format(github_org_repo, version)
+            request = urlopen(Request(url))
+            try:
+                with open(tarball, 'wb') as file_handler:
+                    shutil.copyfileobj(request, file_handler)
+            finally:
+                if sys.version_info[0] == 3:
+                    request.close()
+    with tarfile.open(tarball, 'r:gz') as tar_handler:
+        tar_handler.extractall(str(dest))
+    return dest
 
 
 @pytest.mark.parametrize(
-    ('project'),
+    'call',
     [
-        'python-build',
+        None,  # via code
+        [sys.executable, '-m', 'build'],  # module
+        ['pyproject-build'],  # entrypoint
+    ],
+    ids=['code', 'module', 'entrypoint'],
+)
+@pytest.mark.parametrize(
+    'args',
+    [[], ['-x', '--no-isolation']],
+    ids=['isolated', 'no_isolation'],
+)
+@pytest.mark.parametrize(
+    'project',
+    [
+        'build',
         'pip',
         'dateutil',
         'Solaar',
-        os.path.join('flit', 'flit_core'),
-    ],
-)
-@pytest.mark.parametrize(
-    ('args'),
-    [[], ['-x', '--no-isolation']],
-)
-@pytest.mark.parametrize(
-    ('call'),
-    [
-        None,  # via code
-        ['python', '-m', 'build'],  # module
-        ['pyproject-build'],  # entrypoint
+        'flit',
     ],
 )
 @pytest.mark.isolated
-def test_build(tmp_dir, monkeypatch, integration_path, project, args, call):
+def test_build(monkeypatch, project, args, call, tmp_path):
+    monkeypatch.chdir(tmp_path)
     monkeypatch.setenv('SETUPTOOLS_SCM_PRETEND_VERSION', 'dummy')  # for the projects that use setuptools_scm
 
-    if call and call[0] == 'pyproject-build' and 'PYTHONPATH' in os.environ:
-        pytest.skip('Running via PYTHONPATH, so the pyproject-build entrypoint is not available')
-
-    path = os.path.join(integration_path, project)
-    args = [path, '-o', tmp_dir] + args
+    if call and call[0] == 'pyproject-build':
+        exe_name = 'pyproject-build{}'.format('.exe' if os.name == 'nt' else '')
+        exe = os.path.join(os.path.dirname(sys.executable), exe_name)
+        if os.path.exists(exe):
+            call[0] = exe
+        else:
+            pytest.skip('Running via PYTHONPATH, so the pyproject-build entrypoint is not available')
+    path = get_project(project, tmp_path)
+    pkgs = tmp_path / 'pkgs'
+    args = [str(path), '-o', str(pkgs)] + args
 
     if call is None:
         build.__main__.main(args)
     else:
         subprocess.check_call(call + args)
 
-    assert filter(_SDIST.match, os.listdir(tmp_dir))
-    assert filter(_WHEEL.match, os.listdir(tmp_dir))
+    pkg_names = os.listdir(str(pkgs))
+    assert filter(_SDIST.match, pkg_names)
+    assert filter(_WHEEL.match, pkg_names)
 
 
 def test_isolation(tmp_dir, test_flit_path, mocker):
