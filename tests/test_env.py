470abce090c49ea86be8d0ce8970e0ee5bd1a647
bgabor8 <bgabor8@bloomberg.net>
2020-10-22 10:24:39 +0100

    Disable pytest-xdist for Windows pypy2 - not supported
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- tests/test_env.py
+++ tests/test_env.py
@@ -76,7 +76,6 @@ def test_create_isolated_build_has_with_pip(tmp_path, capfd, mocker):
 
 @pytest.mark.skipif(sys.version_info[0] == 2, reason='venv module used on Python 3 only')
 def test_fail_to_get_script_path(mocker):
-    sysconfig.get_config_vars()  # the config vars are globally cached and may use get_path, make sure they are created
     get_path = mocker.patch('sysconfig.get_path', return_value=None)
     with pytest.raises(RuntimeError, match="Couldn't get environment scripts path"):
         env = build.env.IsolatedEnvBuilder()
@@ -88,7 +87,6 @@ def test_fail_to_get_script_path(mocker):
 
 @pytest.mark.skipif(sys.version_info[0] == 2, reason='venv module used on Python 3 only')
 def test_executable_missing_post_creation(mocker):
-    sysconfig.get_config_vars()  # the config vars are globally cached and may use get_path, make sure they are created
     original_get_path = sysconfig.get_path
 
     def _get_path(name, vars):  # noqa
