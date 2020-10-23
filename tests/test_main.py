1d63c822b5ed3c3db68894f0bc481f14e752af6f
Filipe Laíns <lains@archlinux.org>
2020-10-23 16:34:48 +0100

    main: rename build to build_package
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- tests/test_main.py
+++ tests/test_main.py
@@ -66,10 +66,10 @@ out = os.path.join(cwd, 'dist')
     ],
 )
 def test_parse_args(mocker, cli_args, build_args):
-    mocker.patch('build.__main__.build')
+    mocker.patch('build.__main__.build_package')
 
     build.__main__.main(cli_args)
-    build.__main__.build.assert_called_with(*build_args)
+    build.__main__.build_package.assert_called_with(*build_args)
 
 
 def test_prog():
@@ -92,7 +92,7 @@ def test_build_isolated(mocker, test_flit_path):
     mocker.patch('build.__main__._error')
     install = mocker.patch('build.env._IsolatedEnvVenvPip.install')
 
-    build.__main__.build(test_flit_path, '.', ['sdist'])
+    build.__main__.build_package(test_flit_path, '.', ['sdist'])
 
     build_cmd.assert_called_with('sdist', '.')
     install.assert_called_with({'flit_core >=2,<3'})
@@ -103,7 +103,7 @@ def test_build_no_isolation_check_deps_empty(mocker, test_flit_path):
     build_cmd = mocker.patch('build.ProjectBuilder.build')
     mocker.patch('build.ProjectBuilder.check_dependencies', return_value=[])
 
-    build.__main__.build(test_flit_path, '.', ['sdist'], isolation=False)
+    build.__main__.build_package(test_flit_path, '.', ['sdist'], isolation=False)
 
     build_cmd.assert_called_with('sdist', '.')
 
@@ -114,7 +114,7 @@ def test_build_no_isolation_with_check_deps(mocker, test_flit_path):
     build_cmd = mocker.patch('build.ProjectBuilder.build')
     mocker.patch('build.ProjectBuilder.check_dependencies', return_value=['something'])
 
-    build.__main__.build(test_flit_path, '.', ['sdist'], isolation=False)
+    build.__main__.build_package(test_flit_path, '.', ['sdist'], isolation=False)
 
     build_cmd.assert_called_with('sdist', '.')
     error.assert_called_with('Missing dependencies:\n\tsomething')
@@ -126,7 +126,7 @@ def test_build_raises_build_exception(mocker, test_flit_path):
     mocker.patch('build.ProjectBuilder.build', side_effect=build.BuildException)
     mocker.patch('build.env._IsolatedEnvVenvPip.install')
 
-    build.__main__.build(test_flit_path, '.', ['sdist'])
+    build.__main__.build_package(test_flit_path, '.', ['sdist'])
 
     error.assert_called_with('')
 
@@ -137,6 +137,6 @@ def test_build_raises_build_backend_exception(mocker, test_flit_path):
     mocker.patch('build.ProjectBuilder.build', side_effect=build.BuildBackendException)
     mocker.patch('build.env._IsolatedEnvVenvPip.install')
 
-    build.__main__.build(test_flit_path, '.', ['sdist'])
+    build.__main__.build_package(test_flit_path, '.', ['sdist'])
 
     error.assert_called_with('')
