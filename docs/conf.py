648d76576e1747b8b806c6268793ee3754f2058f
layday <layday@protonmail.com>
2021-10-30 10:46:20 +0300

    docs: show items in the order they are defined

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ html_title = f'build {version}'
 # html_static_path = ['_static']
 
 autoclass_content = 'both'
-
+autodoc_member_order = 'bysource'
 autodoc_preserve_defaults = True
 
 set_type_checking_flag = True
