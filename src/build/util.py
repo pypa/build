cc6133fc04445685d212d2427e33937301bcc0f7
Mark Grassi <17365607+markgras@users.noreply.github.com>
2021-12-31 13:49:58 -0500

    Fix various typos

--- src/build/util.py
+++ src/build/util.py
@@ -31,11 +31,11 @@ def project_wheel_metadata(
     """
     Return the wheel metadata for a project.
 
-    Uses the ``prepare_metadata_for_build_wheel`` hook if availablable,
+    Uses the ``prepare_metadata_for_build_wheel`` hook if available,
     otherwise ``build_wheel``.
 
     :param srcdir: Project source directory
-    :param isolated: Wether or not to run invoke the backend in the current
+    :param isolated: Whether or not to run invoke the backend in the current
                      environment or to create an isolated one and invoke it
                      there.
     """
