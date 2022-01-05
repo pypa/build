234f8deba5da5b9774c9f1d0e1a0ebc232ce69cd
layday <layday@protonmail.com>
2022-01-05 08:14:31 +0200

    fixup! env: prevent warning from being emitted on venv creation

--- src/build/env.py
+++ src/build/env.py
@@ -92,8 +92,11 @@ class IsolatedEnvBuilder:
 
         :return: The isolated build environment
         """
-        # ``realpath`` is used to prevent warning from being emitted that
-        # the venv location has moved on Windows.
+        # Call ``realpath`` to prevent spurious warning from being emitted
+        # that the venv location has changed on Windows. The username is
+        # DOS-encoded in the output of tempfile - the location is the same
+        # but the representation of it is different, which confuses venv.
+        # Ref: https://bugs.python.org/issue46171
         self._path = os.path.realpath(tempfile.mkdtemp(prefix='build-env-'))
         try:
             # use virtualenv when available (as it's faster than venv)
