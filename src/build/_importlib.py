6bf790ceff7dec06324237f2625771ebb5ac47fc
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-07-03 10:09:43 -0400

    Apply suggestions from code review

--- src/build/_importlib.py
+++ src/build/_importlib.py
@@ -4,7 +4,10 @@ import sys
 if sys.version_info >= (3, 10):
     from importlib import metadata
 else:
-    import importlib_metadata as metadata
+    try:
+        import importlib_metadata as metadata
+    except ModuleNotFoundError:
+        from importlib import metadata
 
 
 __all__ = ['metadata']
