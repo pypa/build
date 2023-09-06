d6138f59f35bef619db9c071a45f9c13956c1571
Henry Schreiner <henryschreineriii@gmail.com>
2023-09-05 21:55:51 -0400

    fix: avoid bug in various patch releases of Python
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/build/_util.py
+++ src/build/_util.py
@@ -71,8 +71,15 @@ if typing.TYPE_CHECKING:
 
 else:
     # Per https://peps.python.org/pep-0706/, the "data" filter will become
-    # the default in Python 3.14.
-    if sys.version_info < (3, 14) and hasattr(tarfile, 'data_filter'):
+    # the default in Python 3.14. The first series of releases with the filter
+    # had a broken filter that could not process symlinks correctly.
+    if (
+        (3, 8, 19) <= sys.version_info < (3, 9)
+        or (3, 9, 19) <= sys.version_info < (3, 10)
+        or (3, 10, 13) <= sys.version_info < (3, 11)
+        or (3, 11, 5) < sys.version_info < (3, 12)
+        or (3, 12) <= sys.version_info < (3, 14)
+    ):
 
         class TarFile(tarfile.TarFile):
             extraction_filter = staticmethod(tarfile.data_filter)
