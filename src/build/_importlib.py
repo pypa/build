3184fe7c325b3dc82a21cead56f7c1cdd72fdeea
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2023-10-17 18:26:07 +0200

    Apply suggestions

--- src/build/_importlib.py
+++ src/build/_importlib.py
@@ -2,7 +2,11 @@ import sys
 
 
 if sys.version_info < (3, 10, 2):
-    import importlib_metadata as metadata
+    try:
+        import importlib_metadata as metadata
+    except ModuleNotFoundError:
+        # helps bootstrapping when dependencies aren't installed
+        from importlib import metadata
 else:
     from importlib import metadata
 
