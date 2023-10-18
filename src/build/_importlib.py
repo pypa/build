3a716624f703083a2ca36bb064fa5a6ec4365321
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2023-10-18 11:24:48 +0200

    Trick mypy

--- src/build/_importlib.py
+++ src/build/_importlib.py
@@ -1,13 +1,13 @@
 import sys
 
 
-if sys.version_info < (3, 10, 2):
+if sys.version_info >= (3, 10, 2):
+    from importlib import metadata  # type: ignore[attr-defined]
+else:
     try:
         import importlib_metadata as metadata
     except ModuleNotFoundError:
         # helps bootstrapping when dependencies aren't installed
-        from importlib import metadata
-else:
-    from importlib import metadata
+        from importlib import metadata  # type: ignore[attr-defined]
 
 __all__ = ['metadata']
