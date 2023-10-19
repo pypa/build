52f11f20aa21b1a49543f4ab094657adf649821b
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2023-10-19 08:49:18 +0200

    Fix import order to work with mypy

--- src/build/_importlib.py
+++ src/build/_importlib.py
@@ -1,13 +1,15 @@
 import sys
 
 
-if sys.version_info >= (3, 10, 2):
-    from importlib import metadata  # type: ignore[attr-defined]
+if sys.version_info < (3, 8):
+    import importlib_metadata as metadata
+elif sys.version_info >= (3, 10, 2):
+    from importlib import metadata
 else:
     try:
         import importlib_metadata as metadata
     except ModuleNotFoundError:
         # helps bootstrapping when dependencies aren't installed
-        from importlib import metadata  # type: ignore[attr-defined]
+        from importlib import metadata
 
 __all__ = ['metadata']
