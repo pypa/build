e3081b6a4181086da7e127121836a8e9d275ca0a
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2023-10-27 03:53:48 +0200

    fix: use importlib_metadata for Python < 3.10.2 (#693)
    * Use importlib_metadata for Python < 3.10.2
    
    * Update tests
    
    * Apply suggestions
    
    * Trick mypy
    
    * Fix import order to work with mypy

--- src/build/_importlib.py
+++ src/build/_importlib.py
@@ -3,12 +3,13 @@ import sys
 
 if sys.version_info < (3, 8):
     import importlib_metadata as metadata
-elif sys.version_info < (3, 9, 10) or (3, 10, 0) <= sys.version_info < (3, 10, 2):
+elif sys.version_info >= (3, 10, 2):
+    from importlib import metadata
+else:
     try:
         import importlib_metadata as metadata
     except ModuleNotFoundError:
+        # helps bootstrapping when dependencies aren't installed
         from importlib import metadata
-else:
-    from importlib import metadata
 
 __all__ = ['metadata']
