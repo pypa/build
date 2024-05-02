b71510592e476810f84cdc8b071014ad891dd674
layday <layday@protonmail.com>
2024-05-02 17:26:56 +0300

    build: make compatible with typed `pyproject-hooks`

--- src/build/_types.py
+++ src/build/_types.py
@@ -3,10 +3,9 @@ from __future__ import annotations
 import os
 import typing
 
+from pyproject_hooks import SubprocessRunner as SubprocessRunner
+
 
 ConfigSettings = typing.Mapping[str, typing.Union[str, typing.Sequence[str]]]
 Distribution = typing.Literal['sdist', 'wheel', 'editable']
 StrPath = typing.Union[str, 'os.PathLike[str]']
-SubprocessRunner = typing.Callable[
-    [typing.Sequence[str], typing.Optional[str], typing.Optional[typing.Mapping[str, str]]], None
-]
