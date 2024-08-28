9194e8c5a35f404211f3d51ac2f2374969369bac
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-08-28 09:57:48 -0600

    Add a few intersphinx links to the Python Packaging User Guide

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,7 @@ extensions = [
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
+    'packaging': ('https://packaging.python.org/en/latest/', None),
 }
 
 # Add any paths that contain templates here, relative to this directory.
