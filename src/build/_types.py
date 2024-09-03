96f4537ad0ae9e0313e1a52776e49fe570abcb42
Henry Schreiner <HenrySchreinerIII@gmail.com>
2024-09-03 10:34:47 -0400

    fix: only import SubprocessRunner when type checking

--- src/build/_types.py
+++ src/build/_types.py
@@ -3,9 +3,15 @@ from __future__ import annotations
 import os
 import typing
 
-from pyproject_hooks import SubprocessRunner as SubprocessRunner
-
+__all__ = ['ConfigSettings', 'Distribution', 'StrPath', 'SubprocessRunner']
 
 ConfigSettings = typing.Mapping[str, typing.Union[str, typing.Sequence[str]]]
 Distribution = typing.Literal['sdist', 'wheel', 'editable']
 StrPath = typing.Union[str, 'os.PathLike[str]']
+
+if typing.TYPE_CHECKING:
+    from pyproject_hooks import SubprocessRunner
+else:
+    SubprocessRunner = typing.Callable[
+        [typing.Sequence[str], typing.Optional[str], typing.Optional[typing.Mapping[str, str]]], None
+    ]
