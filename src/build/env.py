78dcb9fb0ad0582a7ab6563c0cfeb70f26eba117
Michał Górny <mgorny@gentoo.org>
2025-02-18 07:39:58 +0100

    Switch to `find_spec`

--- src/build/env.py
+++ src/build/env.py
@@ -159,9 +159,7 @@ class _PipBackend(_EnvBackend):
         if pip is too old, and True if it can be used.
         """
         # `pip install --python` is nonfunctional on Gentoo debundled pip
-        try:
-            import pip._vendor
-        except ImportError:
+        if importlib.util.find_spec("pip._vendor") is None:
             return False
 
         # Version to have added the `--python` option.
