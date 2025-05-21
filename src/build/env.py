pypa/build

commit 35d86b8d4babfe23a72909d851c920e2e2405946

fix: disable use of `pip install --python` for debundled pip (#861)
* Disable use of `pip install --python` on Gentoo debundled pip

In the debundled pip version distributed on Gentoo systems, `pip install
--python` is nonfunctional since it expects all of pip's dependencies
to be present in the (empty) virtual environment.  Detect this case
by the removal of `pip._vendor` package, and use the "no valid outer
pip" codepath.

Bug: https://bugs.gentoo.org/934922

* Switch to `find_spec`

* Reformat

* Silence warnings from _distutils_hack

* Update src/build/env.py

---------

Co-authored-by: Henry Schreiner <HenrySchreinerIII@gmail.com>

src/build/env.py
@@ -11,6 +11,7 @@ import sys
 import sysconfig
 import tempfile
 import typing
+import warnings
 
 from collections.abc import Collection, Mapping
 
@@ -158,6 +159,15 @@ class _PipBackend(_EnvBackend):
         This checks for a valid global pip. Returns None if pip is missing, False
         if pip is too old, and True if it can be used.
         """
+        # `pip install --python` is nonfunctional on Gentoo debundled pip.
+        # Detect that by checking if pip._vendor` module exists.  However,
+        # searching for pip could yield warnings from _distutils_hack,
+        # so silence them.
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            if importlib.util.find_spec('pip._vendor') is None:
+                return False  # pragma: no cover
+
         # Version to have added the `--python` option.
         return _has_dependency('pip', '22.3')
 

commit 5480b7c505d57b1405bb126e977981bc5e44f8f8

fix: Fix `ModuleNotFoundError` when `pip` is not installed
Fix the exception raised if `pip` is not installed at all:

```
[...]
  File "/usr/lib/python3.14/site-packages/build/env.py", line 168, in _has_valid_outer_pip
    if importlib.util.find_spec('pip._vendor') is None:
       ~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^
  File "<frozen importlib.util>", line 90, in find_spec
ModuleNotFoundError: No module named 'pip'
```

This is a regression from #861 where I did not predict that
`find_spec()` will actually raise when the parent package
is not available. To fix it, just reorder the conditions to make sure
that `pip` is installed first.

src/build/env.py
@@ -159,6 +159,11 @@ class _PipBackend(_EnvBackend):
         This checks for a valid global pip. Returns None if pip is missing, False
         if pip is too old, and True if it can be used.
         """
+
+        # Version to have added the `--python` option.
+        if not _has_dependency('pip', '22.3'):
+            return False
+
         # `pip install --python` is nonfunctional on Gentoo debundled pip.
         # Detect that by checking if pip._vendor` module exists.  However,
         # searching for pip could yield warnings from _distutils_hack,
@@ -168,8 +173,7 @@ class _PipBackend(_EnvBackend):
             if importlib.util.find_spec('pip._vendor') is None:
                 return False  # pragma: no cover
 
-        # Version to have added the `--python` option.
-        return _has_dependency('pip', '22.3')
+        return True
 
     @functools.cached_property
     def _has_virtualenv(self) -> bool: