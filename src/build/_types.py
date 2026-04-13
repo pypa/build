3a4ca33b9a0471504593a65d8492b73f4375a040
Henry Schreiner <henryfs@princeton.edu>
2026-04-13 18:40:25 -0400

    chore: disallow old typing imports
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- src/build/_types.py
+++ src/build/_types.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import collections.abc
 import os
 import typing
 
@@ -16,4 +17,6 @@ TYPE_CHECKING = False
 if TYPE_CHECKING:
     from pyproject_hooks import SubprocessRunner
 else:
-    SubprocessRunner = collections.abc.Callable[[collections.abc.Sequence[str], str | None, collections.abc.Mapping[str, str] | None], None]
+    SubprocessRunner = collections.abc.Callable[
+        [collections.abc.Sequence[str], str | None, collections.abc.Mapping[str, str] | None], None
+    ]
